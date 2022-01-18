2ed78cf29d7d21369d64099ad385356ab6c8375f
GWeightman <g.weightman@hotmail.co.uk>
2022-01-18 10:13:41 +0000

    first commit

---
+++ src/user_info/user_model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+
+const user_schema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstname: {
+        type: String,
+        required: true,
+    },
+    surname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+    },
+})
+
+
+user_schema.methods.generate_authtoken = function () {
+    return jwt.sign({_id: this._id}, process.env.SECRET)
+}
+
+const User = mongoose.model("User Info", user_schema)
+
+module.exports = User
