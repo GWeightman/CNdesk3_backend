74ab6f28d383003298bf7cfa55cc734d7aa44612
GWeightman <g.weightman@hotmail.co.uk>
2022-01-24 17:03:03 +0000

    new user route

--- src/user_info/user_routes.js
+++ src/user_info/user_routes.js
@@ -2,14 +2,14 @@ const { Router } = require("express");
 const { hash_compare } = require("../middleware/compare");
 const { hash_pass } = require("../middleware/hash");
 const user_router = Router()
-const { add_user, delete_user, update_user, list_user, list_all_users } = require("./user_controller")
+const { add_user, delete_user, update_user, list_user, list_all_users, list_all_status } = require("./user_controller")
 
 user_router.post("/user", hash_pass, add_user);
 user_router.delete("/user/:username", delete_user)
 user_router.put("/user", hash_pass, update_user)
 user_router.post("/user/:username", list_user)
 user_router.get("/user", list_all_users)
-user_router.get("/user/status", list_all_status)
+user_router.get("/user/:status", list_all_status)
 
 
 
